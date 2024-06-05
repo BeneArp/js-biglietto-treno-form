ccf902819db3a9fde96862526f0f9edc44426cc5
BeneArp <benedetta.arpino@gmail.com>
2024-06-05 16:30:27 +0200

    algoritmo

--- js/script.js
+++ js/script.js
@@ -17,74 +17,86 @@
 
 
 
-// variabili indefinite
-let prezzoScontoMinori;
-let prezzoScontoEta;
-
-
-
-
-// // RACCOLTA DATI UTENTE
-// // chiedere all'utente il numero di chilometri da percorrere
-// const chilometriDaPercorrere = parseInt(prompt("Inserisci il numero di chilomteri da percorrere"));
-
-// // stampo in console il numero di km registrati
-// console.log(chilometriDaPercorrere);
-
-
-
-// // chiedere all'utente la sua età
-// const anniUtente = parseInt(prompt("Inserisci la tua età"));
-
-// // stampo in console l'età dell'utente
-// console.log(anniUtente);
+// predo dal documento il bottone 'Genera'
+const bottoneCalcolo = document.querySelector(".calcolo");
+// prendo dal documento il form per il campo chilometri da percorrere
+const chilometriDaPercorrere = document.getElementById("chilometri");
+// prendo dal documento il form per il campo anni
+const anniUtente = document.getElementById("anni");
 
 
 
 
 
+// evento legato al click sul bottone 'Genera'
+bottoneCalcolo.addEventListener('click',
+
+    function(){
+        // lettura valore nel form chilometri da percorrere
+        let formValueChilomteri = parseInt(chilometriDaPercorrere.value);
+        // stampo in console
+        console.log(formValueChilomteri);
+
+        // lettura valore nel form anni utente
+        let formValueAnni = anniUtente.value;
+        // stampo in console
+        console.log(formValueAnni);
+
+
+        // CALCOLO PREZZO BASE SENZA SCONTI
+        // moltiplicare i chilometri inseriti dall'utente per 0.21€ (prezzo al km)
+        let prezzoBiglietto = (formValueChilomteri * 0.21);
+        // stampo in console
+        console.log("prezzo intero " ,prezzoBiglietto);
+
+        // output prezzo base
+        document.getElementById("prezzo").innerHTML = ("Il prezzo del tuo biglietto è: " + (prezzoBiglietto).toFixed(2) + "€");
 
 
 
-// CALCOLO PREZZO BASE SENZA SCONTI
-// moltiplicare i chilometri inseriti dall'utente per 0.21€ (prezzo al km)
-let prezzoBiglietto = (chilometriDaPercorrere * 0.21);
+        
+        // CALCOLO PREZZO SCONTATO SE L'UTENTE MINORENNE
+        // applicare lo sconto del 20% se l'utente ha meno di 18 anni
+        if (formValueAnni === "minorenne"){
+            let scontoMinori = ((prezzoBiglietto * 20) / 100);
+            let prezzoScontoMinori = (prezzoBiglietto - scontoMinori);
+            // stampo in console
+            console.log("prezzo sconto minorenni " ,prezzoScontoMinori);
 
-// stampo in console il prezzo del biglietto senza sconti 
-console.log("Il prezzo del biglietto è: ",prezzoBiglietto + "€");
-
-// output prezzo base
-document.getElementById("prezzo").innerHTML = ("Il prezzo del tuo biglietto è: " + (prezzoBiglietto).toFixed(2) + "€");
-
-
-
-
+            // output prezzo minori
+            document.getElementById("prezzo").innerHTML = ("Il prezzo del tuo biglietto con lo sconto per i Minorenni è: " + (prezzoScontoMinori).toFixed(2) + "€");
 
 
-// CALCOLO PREZZO SCONTATO SE L'UTENTE MINORENNE
-// applicare lo sconto del 20% se l'utente ha meno di 18 anni
-if (anniUtente < 18){
-    let scontoMinori = ((prezzoBiglietto * 20) / 100);
-    prezzoScontoMinori = (prezzoBiglietto - scontoMinori);
+            // CALCOLO PREZZO SCONTATO SE L'UTENTE >= 65 ANNI
+            // applicare lo sconto del 40% se l'utente ha più di 65 anni
+        } else if (formValueAnni === "65 >="){
+            let scontoEta = ((prezzoBiglietto * 40)/100);
+            let prezzoScontoEta = (prezzoBiglietto - scontoEta);
+            // stampo in console
+            console.log("prezzo sconto età " ,prezzoScontoEta);
 
-    // output prezzo minori
-    document.getElementById("prezzo").innerHTML = ("Il prezzo del tuo biglietto con lo sconto per i Minorenni è: " + (prezzoScontoMinori).toFixed(2) + "€");
-
-    // CALCOLO PREZZO SCONTATO SE L'UTENTE >= 65 ANNI
-    // applicare lo sconto del 40% se l'utente ha più di 65 anni
-} else if (anniUtente >= 65){
-    let scontoEta = ((prezzoBiglietto * 40)/100);
-    prezzoScontoEta = (prezzoBiglietto - scontoEta);
-
-    // output prezzo >= 65 anni
-    document.getElementById("prezzo").innerHTML = ("Il prezzo del tuo biglietto con lo sconto dai 65 anni in su è: " + (prezzoScontoEta).toFixed(2) + "€");
-}
-
-
-// stampo in console il prezzo scontato del 20%
-console.log("Il prezzo con lo sconto per minorenni è: ",prezzoScontoMinori + "€");
+            // output prezzo >= 65 anni
+            document.getElementById("prezzo").innerHTML = ("Il prezzo del tuo biglietto con lo sconto dai 65 anni in su è: " + (prezzoScontoEta).toFixed(2) + "€");
+        }
+    }
+);
 
 
 
-// stampo in console il prezzo scontato del 40%
-console.log("Il prezzo con lo sconto dai 65 anni in su è: ",prezzoScontoEta + "€");
+
+
+
+
+
+
+
+
+
+
+// // stampo in console il prezzo scontato del 20%
+// console.log("Il prezzo con lo sconto per minorenni è: ",prezzoScontoMinori + "€");
+
+
+
+// // stampo in console il prezzo scontato del 40%
+// console.log("Il prezzo con lo sconto dai 65 anni in su è: ",prezzoScontoEta + "€");
